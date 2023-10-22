b9de8f451c2920c91e5a717ee92a0d7612479a20
Richard <campion1boy@gmail.com>
2023-10-22 15:51:16 -0500

    Exercise 2

--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -1,10 +1,27 @@
 document.addEventListener('DOMContentLoaded', (event) => {
 
     var gameboard = document.getElementById("board").children;
+    let gameTrack = [];
+    let turn = 0;
 
     for (let i=0; i<gameboard.length; i++){
         const element = gameboard[i];
         element.setAttribute("class", "square");
+
+
+        element.addEventListener("click", function(){
+            if (turn % 2 == 0){
+                element.textContent = "X";
+                element.classList.add("square", "X");
+                gameTrack.push(i);
+            }else{
+                element.textContent = "0";
+                element.classList.add("square", "O");
+                gameTrack.push(i);
+            }
+            turn++;
+        });
+        
     }
 
 });
